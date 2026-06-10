77a2bdc0b4b2f17d8f1fd1e21231301696791c63
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2026-06-10 18:55:51 +0530

    fix file path

--- resources/js/pages/Profile/Edit.vue
+++ resources/js/pages/Profile/Edit.vue
@@ -4,7 +4,7 @@ import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 import { Head } from '@inertiajs/vue3';
-import { update as updateMember } from "@/routes/teams/members/index.ts";
+import { update as updateMember } from "@/routes/teams/members/index.js";
 
 defineProps({
     mustVerifyEmail: {
--- resources/js/pages/settings/Profile.vue
+++ resources/js/pages/settings/Profile.vue
@@ -10,6 +10,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { edit } from '@/routes/profile/index';
 import { send } from '@/routes/verification/index';
+import { update as updateMember } from "@/routes/teams/members/index.js";
 
 type Props = {
     mustVerifyEmail: boolean;
